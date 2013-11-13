683414a4679f2316f768d0975f6b3b4fb609b642
Neuromatic <neuromatic@archlinux.info>
2013-11-13 01:27:49 +0100

    added bar

---
+++ bar/config.h
@@ -0,0 +1,28 @@
+/* The height of the bar (in pixels) */
+#define BAR_HEIGHT  14
+/* The width of the bar. Set to -1 to fit screen */
+#define BAR_WIDTH   -1
+/* Offset from the left. Set to 0 to have no effect */
+#define BAR_OFFSET 0
+/* Choose between an underline or an overline */
+#define BAR_UNDERLINE 1
+/* The thickness of the underline (in pixels). Set to 0 to disable. */
+#define BAR_UNDERLINE_HEIGHT 2
+/* Default bar position, overwritten by '-b' switch */
+#define BAR_BOTTOM 0
+/* The fonts used for the bar, comma separated. Only the first 2 will be used. */
+#define BAR_FONT        "-*-termsyn-medium-r-*-*-12-*-*-*-*-*-*-*", "fixed"
+/* Some fonts don't set the right width for some chars, pheex it */
+#define BAR_FONT_FALLBACK_WIDTH 6
+/* Color palette */
+#define COLOR0  0x181512 /* background */
+#define COLOR1  0xbea492 /* foreground */
+#define COLOR2  0x8c644c /* red */
+#define COLOR3  0xc4be90 /* green */
+#define COLOR4  0xbfba92 /* yellow */
+#define COLOR5  0x293033 /* blue */
+#define COLOR6  0x6d6871 /* magenta */
+#define COLOR7  0x3b484a /* cyan */
+#define COLOR8  0x504339 /* white */
+#define COLOR9  0x332d29 /* black */
+
